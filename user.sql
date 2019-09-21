0dd3fc313ee21453c74fce36199203c64150913e
Dane Johnson <dane@wustl.edu>
2019-09-21 11:55:34 -0500

    Deleted extra users script

--- user.sql
+++
@@ -1,8 +0,0 @@
-use news;
-create table users(
-    username varchar(50) not null,
-    userId integer not null auto_increment,
-    userPass varchar(255) not null,
-    unique key idx_username (username),
-    primary key (userId)
-) engine = InnoDB default character set = utf8 collate = utf8_general_ci;

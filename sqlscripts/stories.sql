e85abef67d08ecdc15a278c66ef1a994ec879a69
Dane Johnson <dane@wustl.edu>
2019-09-21 11:42:52 -0500

    Fix stories dangling comma

--- sqlscripts/stories.sql
+++ sqlscripts/stories.sql
@@ -4,6 +4,5 @@ create table stories(
     storyId integer not null auto_increment,
     poster integer not null,
     primary key (storyId),
-    foreign key (poster) references user (userId),
-
+    foreign key (poster) references user (userId)
 ) engine = InnoDB default character set = utf8 collate = utf8_general_ci;

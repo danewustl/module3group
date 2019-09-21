76b9bc6ebd626f44c84608ce8a635fa33b4446b0
Dane Johnson <dane@wustl.edu>
2019-09-21 16:59:02 -0500

    Can delete your own story

--- src/news.php
+++ src/news.php
@@ -3,16 +3,25 @@ include "./guard.php";
 include "./database.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $url = $_POST['url'];
-  $stmt = $mysqli->prepare("insert into stories (link, poster) values (?, ?)");
-  $stmt->bind_param("sd", $url, $user);
-  $stmt->execute();
-  $stmt->close();
+  $action = $_POST['action'];
+  if ($action == 'post_story') {
+    $url = $_POST['url'];
+    $stmt = $mysqli->prepare("insert into stories (link, poster) values (?, ?)");
+    $stmt->bind_param("sd", $url, $user);
+    $stmt->execute();
+    $stmt->close();
+  } elseif ($action == 'delete_story') {
+    $storyId = $_POST['story_id'];
+    $stmt = $mysqli->prepare("delete from stories where storyId=?");
+    $stmt->bind_param("d", $storyId);
+    $stmt->execute();
+    $stmt->close();
+  }
 }
 
-$stmt = $mysqli->prepare("select stories.link, users.username from stories, users where users.userId = stories.poster order by stories.storyId desc");
+$stmt = $mysqli->prepare("select stories.link, users.username, users.userId, stories.storyId from stories, users where users.userId = stories.poster order by stories.storyId desc");
 $stmt->execute();
-$stmt->bind_result($link, $poster);
+$stmt->bind_result($link, $poster, $posterId, $storyid);
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,13 +29,19 @@ $stmt->bind_result($link, $poster);
   <body>
     <form action="./news.php" method="POST">
       <label>URL:</label><input type="text" name="url">
+      <input type="hidden" name="action" value="post_story">
       <input type="submit" value="Post Story">
     </form>
     <ul>
       <?php
       while($stmt->fetch()) {
-        echo "<li><a href=\"$link\">$link</a> by $poster</li>";
+        echo "<li><a href=\"$link\">$link</a> by $poster";
+        if ($posterId == $user) {
+          echo "<form action=./news.php method=POST><input type=hidden name=story_id value=$storyid><input type=hidden name=action value=delete_story><input type=submit value=Delete></form>";
+        }
+        echo "</li>";
       }
       ?>
+    </ul>
   </body>
 </html>

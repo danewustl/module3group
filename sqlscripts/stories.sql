b647a242ee427a4a6683b51188cae7b3e24a87e8
Mauricio Fossas <mauricio.fossas@wustl.edu>
2019-09-22 09:49:48 -0500

    Added construction of title column to stroies

--- sqlscripts/stories.sql
+++ sqlscripts/stories.sql
@@ -6,3 +6,5 @@ create table stories(
     primary key (storyId),
     foreign key (poster) references users (userId)
 ) engine = InnoDB default character set = utf8 collate = utf8_general_ci;
+
+alter table stories add title varchar(100);

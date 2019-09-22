b7a06b2fe0698e858b4c94924a1d0c17464bddbf
Dane Johnson <dane@wustl.edu>
2019-09-22 11:55:42 -0500

    Null user if not logged in

--- src/guard.php
+++ src/guard.php
@@ -1,8 +1,10 @@
 <?php
 // Make sure the user is logged in, and then set the global user
 session_start();
+global $user;
 if ($_SESSION['loggedIn']) {
-  global $user;
   $user = $_SESSION['user'];
+} else {
+  $user = null;
 }
 ?>

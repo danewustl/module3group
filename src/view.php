e4eb25991a4fd8df73f1d0a055b8d0622d356073
Mauricio Fossas <mauricio.fossas@wustl.edu>
2019-09-22 10:56:59 -0500

    Added view.php

---
+++ src/view.php
@@ -0,0 +1,45 @@
+<?php
+include "./guard.php";
+include "./database.php";
+
+$storyId=$_GET['sid'];
+
+$stmt = $mysqli->prepare("select stories.title, users.username, stories.link from stories, users where users.userId = stories.poster and stories.storyId = ?");
+$stmt->bind_param('d', $storyId);
+$stmt->execute();
+$stmt->bind_result($title, $poster, $url);
+$stmt->fetch();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $action = $_POST['action'];
+    if ($action == 'post_comment') {
+      $comment = $_POST['comment'];
+      $stmt = $mysqli->prepare("insert into comments (comment, storyId, commenter) values (?, ?, ?)");
+      
+      $stmt->bind_param("sdd", $comment, $storyId, $user);
+      echo $mysqli->error;
+      $stmt->execute();
+      $stmt->close();
+    } elseif ($action == 'delete_story') {
+      $storyId = $_POST['story_id'];
+      $stmt = $mysqli->prepare("delete from stories where storyId=? and poster=?");
+      $stmt->bind_param("ds", $storyId, $user);
+      $stmt->execute();
+      $stmt->close();
+    }
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <?php include "./header.php" ?>
+  <body>
+    <?php echo "<h1>$title by $poster</h1>";
+   echo " <a href=$url> $url </a>"; ?>
+   <form action=<?php echo "./view.php?sid=$storyId";?> method="POST">
+      <textarea rows="4" cols="50" name="comment"></textarea>
+      <input type="hidden" name="action" value="post_comment">
+      <input type="submit" value="Post Comment">
+    </form>
+
+  </body>
+</html>

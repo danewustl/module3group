8b16299a16a209d890cba3b94b6b0ad800dd4637
Dane Johnson <dane@wustl.edu>
2019-09-25 01:00:55 -0500

    Fix guard

--- src/guard.php
+++ src/guard.php
@@ -2,7 +2,7 @@
 // Make sure the user is logged in, and then set the global user
 session_start();
 global $user;
-if (array_key_exists('loggedIn', $_SESSION)) {
+if (array_key_exists('loggedIn', $_SESSION) and $_SESSION['loggedIn']) {
   $user = $_SESSION['user'];
 } else {
   $user = null;
